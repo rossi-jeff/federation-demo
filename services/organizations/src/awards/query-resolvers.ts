00d2da3af8546f3d74e7c2f0ce367f5af55f2788
Jeff Rossi <Jeffrey.Rossi@snapraise.com>
2022-09-15 17:16:35 -0400

    query resolver

---
+++ services/organizations/src/awards/query-resolvers.ts
@@ -0,0 +1,19 @@
+import { db } from '../db'
+import { QueryResolvers, QueryAwardArgs } from '../../../../generated/graphql'
+import { idArgs } from '../../../utils/check-args'
+
+export const awards: QueryResolvers['awards'] = async () => {
+  return await db.client.award.findMany()
+}
+
+export const award: QueryResolvers['award'] = async (
+  _,
+  args: QueryAwardArgs
+) => {
+  const { id } = idArgs(args)
+  return await db.client.award.findFirst({
+    where: {
+      id: parseInt(id)
+    }
+  })
+}
